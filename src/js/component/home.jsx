72e0026cb99d120ce75f39803cb8ed435949f789
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-29 02:20:48 +0000

    just trying to understand stupid project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [data, setData] = useState(null);
+  
+	useEffect(() => {
+	  fetch("https://playground.4geeks.com/apis/fake/todos/user/")
+		//Parse the response as JSON when it's available
+		//Parse = convert object to another type
+		.then((res) => res.json())
+		.then((data) => setData(data));
+	}, []);
+	
+	console.log(data);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	  <div>
+		<ul>
+		  {data ? data.map((item) => <li key={item.id}>{item}</li>) : <div></div>}
+		</ul>
+	  </div>
 	);
-};
+  }
+  
 
 export default Home;
